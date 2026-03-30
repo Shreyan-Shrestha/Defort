53054358980d5026b7b2acaa48390764e07b4eed
Shreyan Shrestha <shreyan.freelance@gmail.com>
2026-03-30 13:02:11 +0545

    Services Migratation, changed from icon to image

--- database/migrations/2026_02_20_065500_create_services_table.php
+++ database/migrations/2026_02_20_065500_create_services_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     {
         Schema::create('services', function (Blueprint $table) {
             $table->id();
-            $table->string('icon')->nullable();
+            $table->string('image')->nullable();
             $table->string('title')->required();
             $table->text('description')->required();
             $table->timestamps();
